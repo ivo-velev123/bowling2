d7cb27502a35c58e5c89c6564578b5027238f7cd
Ivo-velev <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>
2025-06-11 17:30:23 +0100

    fix logic for strikes and spares

--- bowling.py
+++ bowling.py
@@ -1,11 +1,22 @@
-def calc_score(rounds):
+def calc_score(frames):
     total_score = 0
-    rounds = rounds.replace(" ", "")
-    for i in range(0, len(rounds)):
-        if rounds[i] == "X":
-            total_score += 10
-        elif rounds[i] == "/":
-            total_score += (10 - int(rounds[i-1]))
+    frames = frames.replace(" ", "")
+    #we iterate through the code to get the seperate values for each roll in a frame
+    for i in range(0, len(frames)):
+        #check if the roll is a strike
+        
+        if frames[i] == "X":
+            #if the next roll is a strike consider it a double
+            if frames[i+1] == "X":
+                total_score += 20 + (int(frames[i+2]) + int(frames[i+3]))
+            #otherwise score the strike normally
+            else:
+                total_score += 10 + (int(frames[i+1]) + int(frames[i+2]))
+        #check if the roll is a spare
+        elif frames[i] == "/":
+            #if the roll is a spare calculate the value of the marking and add it to the total score.
+            total_score += (10 - int(frames[i-1])) + int(frames[i+1])
         else:
-            total_score += int(rounds[i])
+            #else just add the number to the total
+            total_score += int(frames[i])
     return total_score
--- bowling_test.py
+++ bowling_test.py
@@ -1,12 +1,20 @@
 import unittest
-from bowling import calc_score
+from bowling import *
 
 class TestCalcScore(unittest.TestCase):
-    def test_does_simple_numbers(self):
+    def test_does_simple_rolls(self):
         self.assertEqual(calc_score("11 11 11 11"), 8)
 
-    def test_does_X(self):
-        self.assertEqual(calc_score("X 11 54 81"), 30)
+    def test_does_strike(self):
+        self.assertEqual(calc_score("X 11 54 81"), 32)
     
-    def test_does_slash(self):
-        self.assertEqual(calc_score("4/ 11 11 X"), 24)
+    def test_does_spare(self):
+        self.assertEqual(calc_score("4/ 11 11 11"), 17)
+    
+    def test_does_double_strike(self):
+        self.assertEqual(calc_score("X X 23 33"), 51)
+
+# class TestHelperMethods(unittest.TestCase):
+#     def test_does_check_X(self):
+#         self.assertEqual(check_Strike("X"), 10)
+    
