857fa37c1bc45d14cbcad2e5536fd386322d1b1b
Ivo-velev <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>
2025-06-12 12:02:09 +0100

    fixed some strike logic

--- bowling.py
+++ bowling.py
@@ -2,24 +2,29 @@ def calc_score(frames):
     total_score = 0
     frames = frames.replace(" ", "")
     #we iterate through the code to get the seperate values for each roll in a frame
-    for i in range(0, len(frames)):
+    for index in range(0, len(frames)):
         #check if the roll is a strike
-        
-        if frames[i] == "X":
+        if frames[index] == "X":
             #if the next roll is a strike consider it a double
-            if i != (len(frames) - 1):
-                if frames[i+1] == "X":
-                    total_score += 20 + (int(frames[i+2]) + int(frames[i+3]))
+            if index != (len(frames) - 1):
+                if frames[index+1] == "X":
+                    total_score += 20 + int(frames[index+2])
                 #otherwise score the strike normally
                 else:
-                    total_score += 10 + (int(frames[i+1]) + int(frames[i+2]))
+                    if frames[index+2] == "/":
+                        total_score += 20
+                    else:
+                        total_score += 10 + (int(frames[index+1]) + int(frames[index+2]))
             else:
                 total_score += 10
         #check if the roll is a spare
-        elif frames[i] == "/":
-            #if the roll is a spare calculate the value of the marking and add it to the total score.
-            total_score += (10 - int(frames[i-1])) + int(frames[i+1])
+        elif frames[index] == "/":
+            if index != (len(frames) - 1):
+                #if the roll is a spare calculate the value of the marking and add it to the total score.
+                total_score += (10 - int(frames[index-1])) + int(frames[index+1])
+            else:
+                total_score += (10 - int(frames[index-1]))
         else:
             #else just add the number to the total
-            total_score += int(frames[i])
+            total_score += int(frames[index])
     return total_score
--- bowling_test.py
+++ bowling_test.py
@@ -12,8 +12,20 @@ class TestCalcScore(unittest.TestCase):
         self.assertEqual(calc_score("4/ 11 11 11"), 17)
     
     def test_does_double_strike(self):
-        self.assertEqual(calc_score("X X 23 33"), 51)
+        self.assertEqual(calc_score("X X 23 33"), 48)
 
+    def test_does_ten_frames(self):
+        self.assertEqual(calc_score("11 X 11 1/ 11 11 11 11 11 11"), 39)
+    
+    def test_does_bonus_rolls(self):
+        self.assertEqual(calc_score("11 X 11 1/ 11 11 11 11 11 X 11"), 51)
+    
+    def test_does_bonus_rolls_with_spare(self):
+        self.assertEqual(calc_score("11 X 11 1/ 11 11 11 11 11 X 1/"), 57)
+        
 class TestEdgeCases(unittest.TestCase):
-    def test_does_end_X(self):
+    def test_does_end_strike(self):
         self.assertEqual(calc_score("11 11 11 X"), 16)
+    
+    def test_does_end_spare(self):
+        self.assertEqual(calc_score("11 11 11 4/"), 16)
